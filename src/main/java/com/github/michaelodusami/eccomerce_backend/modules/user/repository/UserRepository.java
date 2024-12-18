f6d744f4de82094bd63ffa606a49e785d2c61b78
Documentayshin <kyrmikee@gmail.com>
2024-12-17 23:06:42 -0500

    feat: add custom method to repo

--- src/main/java/com/github/michaelodusami/eccomerce_backend/modules/user/repository/UserRepository.java
+++ src/main/java/com/github/michaelodusami/eccomerce_backend/modules/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.github.michaelodusami.eccomerce_backend.modules.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.github.michaelodusami.eccomerce_backend.modules.user.entity.User;
 
 public interface UserRepository extends ListCrudRepository<User, Long> {
     
+    public Optional<User> findByEmail(String email);
 } 
