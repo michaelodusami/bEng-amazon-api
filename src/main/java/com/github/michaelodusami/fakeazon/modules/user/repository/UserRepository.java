0a91b852f7686135676d08afef81e5c8d6eda32c
Documentayshin <kyrmikee@gmail.com>
2024-12-19 19:49:46 -0500

    refactor: add annotation to repository

--- src/main/java/com/github/michaelodusami/fakeazon/modules/user/repository/UserRepository.java
+++ src/main/java/com/github/michaelodusami/fakeazon/modules/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.github.michaelodusami.fakeazon.modules.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.michaelodusami.fakeazon.modules.user.entity.User;
 
+@Repository
 public interface UserRepository extends ListCrudRepository<User, Long> {
     
     public Optional<User> findByEmail(String email);
