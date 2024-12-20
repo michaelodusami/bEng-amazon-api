22f03a49ff3aeed2f77e87757f3b737ad230b4da
Documentayshin <kyrmikee@gmail.com>
2024-12-20 00:38:15 -0500

    refactor: create finding a user by a role

--- src/main/java/com/github/michaelodusami/fakeazon/modules/user/repository/UserRepository.java
+++ src/main/java/com/github/michaelodusami/fakeazon/modules/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.github.michaelodusami.fakeazon.modules.user.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.michaelodusami.fakeazon.modules.user.entity.User;
@@ -11,4 +14,7 @@ import com.github.michaelodusami.fakeazon.modules.user.entity.User;
 public interface UserRepository extends ListCrudRepository<User, Long> {
     
     public Optional<User> findByEmail(String email);
+
+    @Query("SELECT u from User u JOIN u.roles r WHERE r = :role")
+    List<User> findUsersByRole(@Param("role") String role);
 } 
