ff31b96b8aec0fc786868c1743fa3ea80ad18732
Documentayshin <kyrmikee@gmail.com>
2024-12-22 01:44:53 -0500

    refactor: populate enum

--- src/main/java/com/github/michaelodusami/fakeazon/common/enums/AmazonStatus.java
+++ src/main/java/com/github/michaelodusami/fakeazon/common/enums/AmazonStatus.java
@@ -2,4 +2,56 @@ package com.github.michaelodusami.fakeazon.common.enums;
 
 public enum AmazonStatus {
 
+    // General Statuses
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    COMPLETED,
+    FAILED,
+    CANCELLED,
+
+    // User/Customer Related Statuses
+    USER_REGISTERED,
+    USER_VERIFIED,
+    USER_SUSPENDED,
+    CUSTOMER_CREATED,
+    CUSTOMER_UPDATED,
+    CUSTOMER_DELETED,
+
+    // Order Related Statuses
+    ORDER_PLACED,
+    ORDER_CONFIRMED,
+    ORDER_CANCELLED,
+    ORDER_SHIPPED,
+    ORDER_DELIVERED,
+    ORDER_RETURNED,
+    ORDER_REFUNDED,
+
+    // Payment Related Statuses
+    PAYMENT_INITIATED,
+    PAYMENT_COMPLETED,
+    PAYMENT_FAILED,
+    PAYMENT_REFUNDED,
+
+    // Inventory Related Statuses
+    INVENTORY_UPDATED,
+    INVENTORY_LOW,
+    INVENTORY_OUT_OF_STOCK,
+    INVENTORY_REPLENISHED,
+
+    // Shipping/Delivery Related Statuses
+    SHIPMENT_PREPARED,
+    SHIPMENT_IN_TRANSIT,
+    SHIPMENT_DELIVERED,
+    SHIPMENT_DELAYED,
+
+    // Notification Related Statuses
+    NOTIFICATION_SENT,
+    NOTIFICATION_FAILED,
+
+    // Miscellaneous Statuses
+    REVIEW_SUBMITTED,
+    REVIEW_APPROVED,
+    REVIEW_REJECTED
+
 }
--- src/main/java/com/github/michaelodusami/fakeazon/modules/payment/enums/PaymentMethod.java
+++ src/main/java/com/github/michaelodusami/fakeazon/modules/payment/enums/PaymentMethod.java
@@ -2,4 +2,35 @@ package com.github.michaelodusami.fakeazon.modules.payment.enums;
 
 public enum PaymentMethod {
 
+    // Credit/Debit Card Payments
+    VISA,
+    MASTERCARD,
+    AMERICAN_EXPRESS,
+    DISCOVER,
+
+    // Digital Wallets
+    PAYPAL,
+    APPLE_PAY,
+    GOOGLE_PAY,
+    AMAZON_PAY,
+
+    // Bank Transfers
+    ACH,
+    WIRE_TRANSFER,
+    DIRECT_DEBIT,
+
+    // Buy Now, Pay Later
+    AFFIRM,
+    KLARNA,
+    AFTERPAY,
+
+    // Cryptocurrencies
+    BITCOIN,
+    ETHEREUM,
+    LITECOIN,
+
+    // Cash/Other
+    CASH_ON_DELIVERY,
+    CHECK,
+    MONEY_ORDER
 }
